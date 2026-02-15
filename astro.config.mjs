971727e68fcdf7572e358cbf313c3c7605e84454
Jonathan Stewart <going2timbuktu@gmail.com>
2026-02-14 22:02:03 -0600

    Remove unused cloudflare import

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   site: 'https://virtuallyanadmin.com',
