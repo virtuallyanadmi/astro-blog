ad50c773fa36e3fb54cf8ef6a8b6b93790906996
Jonathan Stewart <going2timbuktu@gmail.com>
2026-02-14 21:59:34 -0600

    Configure for Cloudflare Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   site: 'https://virtuallyanadmin.com',
+  output: 'static',
   integrations: [mdx()],
   markdown: {
     shikiConfig: {
